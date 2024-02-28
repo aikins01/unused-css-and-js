4a9c3e98785a86d4c3aebc4f2b2d84bd8f077b00
Aikins Laryea <aikinslaryea@gmail.com>
2024-02-28 07:54:11 +0000

    improvement: added webdriver and loauching correctly

--- src/UnusedResourceAnalyzer.java
+++ src/UnusedResourceAnalyzer.java
@@ -1,5 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class UnusedResourceAnalyzer {
+
+    private static final String WEB_DRIVER_PATH = "lib/web_driver/chromedriver";
+
+    private static final String TARGET_WEBSITE = "https://www.google.com";
+
     public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(TARGET_WEBSITE);
+        driver.quit();
         System.out.println("Hello, World!");
     }
 }

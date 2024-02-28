858f3ad7d144602718550824a1cc46554a966373
Aikins Laryea <aikinslaryea@gmail.com>
2024-02-28 08:05:34 +0000

    feature: added getting all referenced resources on a page and logging

--- src/UnusedResourceAnalyzer.java
+++ src/UnusedResourceAnalyzer.java
@@ -1,3 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -5,14 +12,40 @@ public class UnusedResourceAnalyzer {
 
     private static final String WEB_DRIVER_PATH = "lib/web_driver/chromedriver";
 
-    private static final String TARGET_WEBSITE = "https://www.google.com";
+    private static final String TARGET_WEBSITE = "https://www.ghanaweb.com";
 
     public static void main(String[] args) throws Exception {
         System.setProperty("webdriver.chrome.driver", WEB_DRIVER_PATH);
         WebDriver driver = new ChromeDriver();
 
         driver.get(TARGET_WEBSITE);
+        String html = driver.getPageSource();
         driver.quit();
+
+        Document doc = Jsoup.parse(html);
+
+        // collect all resources
+        Set<String> referencedResources = new HashSet<>();
+        Elements scriptTags = doc.getElementsByTag("script");
+        for (Element scriptTag : scriptTags) {
+            String src = scriptTag.attr("src");
+            if (!src.isEmpty()) {
+                referencedResources.add(src);
+            }
+        }
+        Elements linkTags = doc.getElementsByTag("link");
+        for (Element linkTag : linkTags) {
+            String href = linkTag.attr("href");
+            if (!href.isEmpty() && linkTag.attr("rel").equals("stylesheet")) {
+                referencedResources.add(href);
+            }
+        }
+
+        System.out.println("Referenced resources:");
+        for (String resource : referencedResources) {
+            System.out.println(resource);
+        }
+
         System.out.println("Hello, World!");
     }
 }
